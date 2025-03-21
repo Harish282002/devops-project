3e0744df6428f387aa6c944829c1765019f231fe
Your Full Name <your_email@example.com>
2025-03-21 09:56:59 +0000

    Resolved merge conflict in helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,2 @@
-print("Hello World")
-print("I am learning git")
+git add helloworld.pyprint("Hello World") print("I am learning git") git 
+commit -m "Resolved merge conflict in helloworld.py" cat helloworld.py
